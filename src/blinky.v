54d17b2de1cb0a29b142baa5d461cb2b41bc71a6
wolf <douglaswolf@yahoo.com>
2025-04-01 09:52:00 -0700

    No commit message

---
+++ src/blinky.v
@@ -0,0 +1,22 @@
+module blinky # (FREQ_HZ = 100000000)
+(
+    input clk,
+    output cattrip,
+    output reg led
+);
+
+reg[31:0] counter = 0;
+
+always @(posedge clk) begin
+    
+    if (counter == 0) begin
+        led <= ~led;
+        counter <= FREQ_HZ/8;
+    end
+
+    else counter <= counter - 1;
+end
+
+assign cattrip = 0;
+
+endmodule
